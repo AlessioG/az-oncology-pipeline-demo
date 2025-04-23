2583f72e5979976939b3d6fab87e3e1f4eea784b
= <=>
2025-04-23 15:03:53 +0200

    test

--- blocks/home-grid-actions/home-grid-actions.js
+++ blocks/home-grid-actions/home-grid-actions.js
@@ -4,14 +4,12 @@ export default function decorate(block) {
   block.classList.add('home-grid-actions');
 
   const ul = block.createElement('ul');
-  ul.className("type-actions");
-  const li = block.createElement('li');
-  li.textContent = 'Actions';
-  const li2 = block.createElement('li')
-  li2.textContent = 'Actions2';
-  ul.append(li);
-  ul.append(li2);
-
-  block.textContent = '';
-  block.append(ul);
+  ul.classList.add('type-links');
+  const l1= block.createElement('li');
+  li1.textContent = 'Element 1';
+  const l2= block.createElement('li');
+  l2.textContent = 'Element 2';
+  ul.appendChild(l1);
+  il.appendChild(l2);
+  block.appendChild(ul);
 }
